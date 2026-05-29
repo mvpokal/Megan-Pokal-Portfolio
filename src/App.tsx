2729f5fa034b2150716075b62364ef91e318e4fe
mvpokal <mvpm10@icloud.com>
2026-05-29 15:52:11 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import { Header } from "./components/header/header";
-import { Home } from "./components/home/home";
 import { About } from "./components/about/about";
-import { Projects } from "./components/projects/projects";
-import { Contact } from "./components/contact/contact";
+import { Experience } from "./components/experience/experience";
 import { Footer } from "./components/footer/footer";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <Home />
       <About />
-      <Projects />
-      <Contact />
+      <Experience />
       <Footer />
     </div>
   );
