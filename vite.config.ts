36d2bacb44b9c89f7c0d75837ca510d3da272da8
mvpokal <mvpm10@icloud.com>
2026-05-29 19:53:56 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/Megan-Pokal-Portfolio/',
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+});
